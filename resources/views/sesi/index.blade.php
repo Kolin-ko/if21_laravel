1b7b4e8f0bdaa115f700b9be0002368c70db21af
Kolin-ko <kkolinn4457@gmail.com>
2025-06-02 01:00:03 +0700

    tes

--- resources/views/sesi/index.blade.php
+++ resources/views/sesi/index.blade.php
@@ -30,30 +30,26 @@
                   </div>
                 </div>
                 <div class="card-body">
-                  {{-- <a href="{{ route('prodi.create') }}" type="button" class="btn btn-primary btn-rounded btn-fw"><i class="bi bi-database-add"></i></a>
+                  {{-- <a href="{{ route('sesi.create') }}" type="button" class="btn btn-primary btn-rounded btn-fw"><i class="bi bi-database-add"></i></a>
                   @if (Session::get('success'))
                   <div class="alert alert-success mt-3">{{ Session::get('success') }}</div>
               @endif --}}
                   <table class = 'table'>
                     <thead>
                       <tr>
-                        <th>Nama</th>
-                        <th>Singkatan</th>
-                        <th>Nama Dekan</th>
-                        <th>Nama Wakil Dekan</th>
+                        <th>ID</th>
+                        <th>Nama Sesi</th>
                         <th>Aksi</th>
                       </tr>
                     </thead>
                     <tbody>
-                  {{-- @foreach ($fakultas as $item)
+                  @foreach ($sesi as $item)
                   <tr>
+                  <td>{{ $item->uuid }}</td>
                   <td>{{ $item->nama }}</td>
-                  <td>{{ $item->singkatan }}</td>
-                  <td>{{ $item->nama_dekan }}</td>
-                  <td>{{ $item->nama_wadek }}</td>
-                  <td> --}}
+                  {{-- <td> --}}
                   {{-- <a href="{{ route('fakultas.edit', $item->id) }}" class="btn btn-xs btn-warning"><i class="bi bi-pencil-fill"></i></a>
-                    <form method="POST" action="{{ route('fakultas.destroy', $item->id) }}">
+                    <form method="POST" action="{{ route('sesi.destroy', $item->id) }}">
                       @csrf
                       <input name="_method" type="hidden" value="DELETE">
                       <button type="submit" class="btn btn-xs btn-danger btn-rounded show_confirm"
@@ -61,7 +57,7 @@
                           data-nama='{{ $item->nama }}'><i class="bi bi-trash"></i></button> --}}
                   </td>
                   </tr>
-                  {{-- @endforeach --}}
+                  @endforeach
                     </tbody>
                   </table>
 
