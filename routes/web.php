98842401bc5790190d83c02bad189e09cf035085
Kolin-ko <kkolinn4457@gmail.com>
2025-06-03 18:22:31 +0700

    3/6

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,12 @@ use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MataKuliahController;
 use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\SesiController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
-
-
-
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource('/dashboard', DashboardController::class);
 Route::resource('/fakultas', FakultasController::class);
@@ -21,3 +21,15 @@ Route::resource('/mahasiswa', MahasiswaController::class);
 Route::resource('sesi', SesiController::class);
 Route::resource('/mata_kuliah', MataKuliahController::class);
 Route::resource('/jadwal', JadwalController::class);
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
